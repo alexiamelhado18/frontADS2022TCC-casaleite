e73dadd618e8ed7485a6c353afe36afa2ae02aeb
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-09-07 22:50:57 -0300

    created component

---
+++ src/components/Produto.vue
@@ -0,0 +1,32 @@
+<template>
+  <tr>
+    <td>{{ id }}</td>
+    <td>{{ nome }}</td>
+    <td>
+      <router-link :to="{ name: 'AlterarProduto' }">
+        <b-icon class="icon" icon="pencil" aria-hidden="true"></b-icon>
+      </router-link>
+
+      <b-icon
+        class="icon"
+        icon="x"
+        aria-hidden="true"
+        @click="$emit('setarItemExclusao')"
+      ></b-icon>
+    </td>
+  </tr>
+</template>
+<script>
+export default {
+  name: "ComponentProduto",
+  props: {
+    nome: String,
+    id: Number,
+  },
+};
+</script>
+<style scoped>
+.icon {
+  color: #000;
+}
+</style>
