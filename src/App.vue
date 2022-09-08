ccfa5dd88ad3d1450b072fa3ccff8fb662986b9b
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-09-07 22:49:51 -0300

    project pattern files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav> -->
+    <router-view />
+  </div>
+</template>
+<style scoped>
+#app {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+}
+</style>
