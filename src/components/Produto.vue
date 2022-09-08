a070156dfadccf58c1fee228716220b68517de77
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-09-08 00:55:39 -0300

    inclusion of new props and new function that displays the product description

--- src/components/Produto.vue
+++ src/components/Produto.vue
@@ -1,7 +1,11 @@
 <template>
   <tr>
     <td>{{ id }}</td>
-    <td>{{ nome }}</td>
+    <td>{{ nome + color }}</td>
+    <td>{{ marca }}</td>
+    <td class="link-descricao" @click="$emit('verDescricao')">ver descrição</td>
+    <td>{{ data_expiracao }}</td>
+    <td>{{ estoque }}</td>
     <td>
       <router-link :to="{ name: 'AlterarProduto' }">
         <b-icon class="icon" icon="pencil" aria-hidden="true"></b-icon>
@@ -20,13 +24,31 @@
 export default {
   name: "ComponentProduto",
   props: {
-    nome: String,
     id: Number,
+    nome: String,
+    marca: String,
+    color: {
+      type: String,
+      default: " Vermelho",
+    },
+    estoque: {
+      type: Number,
+      required: true,
+    },
+    data_expiracao: String,
+    descricao: {
+      type: String,
+      default: "lorem lorem lorem....",
+    },
   },
 };
 </script>
 <style scoped>
 .icon {
   color: #000;
+}
+.link-descricao {
+  text-decoration: underline;
+  cursor: pointer;
 }
 </style>
