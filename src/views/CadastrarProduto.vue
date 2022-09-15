b39298be313377b171e8df6f86071148905e906f
henriqueyda <henrique.ydaa@gmail.com>
2022-09-14 23:28:21 -0300

    feat: adiciona cadastrar produtos

--- src/views/CadastrarProduto.vue
+++ src/views/CadastrarProduto.vue
@@ -5,6 +5,7 @@
     <h2 class="mb-5 mt-3">Cadastrar produto</h2>
     <b-form
       class="d-flex justify-content-center align-items-start h-100 flex-wrap w-100"
+      @submit.prevent="cadastrarProduto"
     >
       <div
         class="d-flex flex-column justify-content-between align-items-start m-2"
@@ -16,7 +17,13 @@
           label="Nome:"
           label-for="input-1"
         >
-          <b-form-input id="input-1" class="mb-2 w-100" type="text" required>
+          <b-form-input
+            id="input-1"
+            class="mb-2 w-100"
+            type="text"
+            required
+            v-model="item.name"
+          >
           </b-form-input>
         </b-form-group>
 
@@ -26,7 +33,13 @@
           label="Marca:"
           label-for="input-1"
         >
-          <b-form-input id="input-1" class="mb-2 w-100" type="text" required>
+          <b-form-input
+            id="input-1"
+            class="mb-2 w-100"
+            type="text"
+            required
+            v-model="item.brand"
+          >
           </b-form-input>
         </b-form-group>
 
@@ -36,7 +49,13 @@
           label="Cor:"
           label-for="input-1"
         >
-          <b-form-input id="type-color" class="mb-2 w-100" type="text" required>
+          <b-form-input
+            id="type-color"
+            class="mb-2 w-100"
+            type="text"
+            required
+            v-model="item.color"
+          >
           </b-form-input>
         </b-form-group>
       </div>
@@ -55,6 +74,7 @@
             class="mb-2 w-100"
             type="number"
             required
+            v-model="item.quantity"
           >
           </b-form-input>
         </b-form-group>
@@ -69,6 +89,7 @@
             id="textarea"
             placeholder="Enter something..."
             class="mb-2 w-100"
+            v-model="item.description"
           >
           </b-form-textarea>
         </b-form-group>
@@ -79,13 +100,24 @@
           label-for="input-2"
           class="mb-2 w-100"
         >
-          <b-form-input id="type-date" type="date"></b-form-input>
+          <b-form-input
+            id="type-date"
+            type="date"
+            v-model="item.expiration_date"
+          >
+          </b-form-input>
         </b-form-group>
         <div class="d-flex justify-content-end align-items-center w-100">
           <b-button type="submit" variant="primary">Cadastrar</b-button>
         </div>
       </div>
     </b-form>
+    <b-modal
+      ref="modal-confirmar-cadastro"
+      title="Produto Cadastrado com Sucesso!"
+      hide-footer
+    >
+    </b-modal>
     <b-modal ref="modal-status" hide-footer>
       <!-- Mostrar msg do response da api -->
     </b-modal>
@@ -95,5 +127,33 @@
 <script>
 export default {
   name: "CadastrarProduto",
+  data() {
+    return {
+      item: {},
+      status: "",
+    };
+  },
+  methods: {
+    cadastrarProduto() {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.item),
+      };
+      fetch("http://127.0.0.1:5000/product/", requestOptions)
+        .then((response) => {
+          if (response.status === 201) {
+            this.status = "Seu produto foi cadastrado com sucesso!";
+            this.abrirModal();
+          } else {
+            this.status = "Não foi possível cadastrar esse produto!";
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+    abrirModal() {
+      this.$refs["modal-confirmar-cadastro"].show();
+    },
+  },
 };
 </script>
