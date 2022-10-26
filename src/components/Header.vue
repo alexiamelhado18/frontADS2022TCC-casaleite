7d5b09f9d2468f4ca96bda52388a557e353e3f2d
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-10-25 23:42:46 -0300

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header
+    class="w-100 d-flex justify-content-between align-items-center"
+    style="height: 100px; background-color: #6c757d; padding: 2%"
+  >
+    <RouterLink
+      class="text-decoration-none"
+      :to="{ name: '' }"
+      style="color: #fff"
+      title="Casa Leite"
+    >
+      <img
+        style="border-radius: 100%"
+        :src="require('@/assets/images/logo-casaleite.jpg')"
+        alt=""
+      />
+    </RouterLink>
+    <nav class="w-100 d-flex justify-content-center align-items-center">
+      <RouterLink
+        class="text-decoration-none"
+        :to="{ name: 'Produtos' }"
+        style="color: #fff"
+        >Materias</RouterLink
+      >
+    </nav>
+  </header>
+</template>
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+  name: "NavBar",
+  components: { RouterLink },
+};
+</script>
