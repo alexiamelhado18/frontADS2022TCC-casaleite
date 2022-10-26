f7829a10e82059fa3b4095c6611b62b208475c19
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-10-25 23:43:03 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <Header />
+
     <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,12 +9,20 @@
     <router-view />
   </div>
 </template>
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "App",
+  components: { Header },
+};
+</script>
 <style scoped>
 #app {
   display: flex;
   align-items: center;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-start;
   width: 100%;
   min-height: 100vh;
 }
